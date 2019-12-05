07f8c1670823cb845d1bf4e538b693ccd07c341f
James Tu <james.tu@starz.com>
2019-12-04 22:32:45 -0800

    boot  strap file

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// we are targing the browser ... angular can target multiple platforms
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
